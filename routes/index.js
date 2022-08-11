dfb059fdb59c5e971d91a1d9c69bb1b6244333b0
Jim Haff <Jimmyhaff3@gmail.com>
2022-08-11 14:41:23 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
-var router = require('express').Router();
+const router = require('express').Router();
 const passport = require('passport');
 
 // The root route renders our only view
 router.get('/', function(req, res) {
+  //UPDATE THIS
   // Where do you want to go for the root route
-  // in the student demo this was res.redirect('/students'), what do you want?
+  // in the student demo this was res.redirect('/movies'), what do you want?
   // This could be a landing page, or just redirect to your main resource page which you'll have an a tag that makes 
   // a request to `/auth/google` route below
 });
@@ -19,15 +20,16 @@ router.get('/auth/google', passport.authenticate(
 router.get('/oauth2callback', passport.authenticate(
   'google',
   {
-    successRedirect : '/', // where do you want the client to go after you login 
-    failureRedirect : '/' // where do you want the client to go if login fails
+    successRedirect : '/', // UPDATE THIS, where do you want the client to go after you login 
+    failureRedirect : '/' //  UPDATE THIS, where do you want the client to go if login fails
   }
 ));
 
 // OAuth logout route
 router.get('/logout', function(req, res){
-  req.logout();
-  res.redirect('/');
-});
+  req.logout(function(){ //< - req.logout comes from passport, and what it does is destorys the cookie keeping track of the user!
+    res.redirect('/'). // <---- UPDATE THIS TO WHERE YOU WANT THE USER TO GO AFTER LOGOUT
+  })
+})
 
 module.exports = router;
